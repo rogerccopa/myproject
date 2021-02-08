9e2dc7934e400f1bdf432f291a8813dd45a61cbd
Roger Ccopa <rogerccopa@gmail.com>
2021-02-08 10:28:27 -0500

    First commit

---
+++ src/Navbar.vue
@@ -0,0 +1,20 @@
+<template>
+    <nav>
+        <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">About</a></li>
+            <li><a href="">Contact</a></li>
+        </ul>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: 'Navbar',
+    data(){
+        return {
+            
+        }
+    }
+}
+</script>
