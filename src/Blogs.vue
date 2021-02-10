d04f147b24d73e4f909e568f0a88513b94dc4b54
Roger Ccopa <rogerccopa@gmail.com>
2021-02-09 21:29:57 -0500

    Filters

--- src/Blogs.vue
+++ src/Blogs.vue
@@ -4,7 +4,7 @@
         <button @click="changeTitle">Change Title</button>
         <div v-for="post in posts" :key="post.id">
             <h3>{{post.title}}</h3>
-            <p>{{post.body}}</p>
+            <p>{{ post.body | snippet }}</p>
         </div>
     </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
+Vue.filter('snippet', val => {
+  if (!val || typeof(val) != 'string') return '';
+  val = val.slice(0,50);
+  return val;
+})
 
 new Vue({
   render: h => h(App),
