e424114ac6a344091cd945e20504b8c985ecc717
Roger Ccopa <rogerccopa@gmail.com>
2021-02-09 21:01:18 -0500

    Making API request with Axios

--- src/Blogs.vue
+++ src/Blogs.vue
@@ -2,15 +2,22 @@
     <div class="blogs">
         <h2>{{blogTitle}}</h2>
         <button @click="changeTitle">Change Title</button>
+        <div v-for="post in posts" :key="post.id">
+            <h3>{{post.title}}</h3>
+            <p>{{post.body}}</p>
+        </div>
     </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
     name: 'Blogs',
     data(){
         return {
-            blogTitle:'Blog'
+            blogTitle:'Blog',
+            posts:[]
         }
     },
     methods:{
@@ -18,14 +25,14 @@ export default {
             this.blogTitle = 'Amazing Blog Site';
         }
     },
-    beforeCreate(){
-        alert('beforeCreate hook');
-    },
     created(){
-        alert('created hook');
-    },
-    beforeUpdate(){
-        alert('beforeUpdate hook');
+        axios.get('https://jsonplaceholder.typicode.com/posts/')
+        .then(response => {
+            this.posts = response.data;
+        })
+        .catch(err => {
+            console.log(err);
+        })
     }
 }
 </script>
